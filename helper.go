b72fb8e985cac3998eb76a89d30ca6f839a82889
Christian Arnheiter <christian.arnheiter@nttdata.com>
2020-11-02 12:31:59 +0100

    rest interface added

--- helper.go
+++ helper.go
@@ -1,26 +1,8 @@
 package mongo
 
 import (
-	"log"
 	"net/http"
 )
-
-func check(condition func() bool, w http.ResponseWriter) bool {
-	if condition() {
-		http.Error(w, "BadRequest", http.StatusBadRequest)
-		return true
-	}
-	return false
-}
-
-func checkError(err error, w http.ResponseWriter) bool {
-	return check(func() bool {
-		if err != nil {
-			log.Println(err)
-		}
-		return err != nil
-	}, w)
-}
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
